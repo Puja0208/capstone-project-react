0fc577b2798c7e2b36848154e186d426b0e9d2a2
Puja0208 <pooja2081997@gmail.com>
2021-12-21 13:52:02 +0530

    fix: Display image issue fixed

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,18 +1,16 @@
 import React, { useContext } from 'react';
 
 import Image from '../components/Image';
+import { Context } from '../Context';
 import { getClass } from '../utils';
 
-import { Context } from '../Context';
-
 function Photos() {
-  // Get the allPhotos array from context
-  // map over it, creating <Image /> elements of the component we just made
-  // <Image key={???} img={<full image object here>} className={getClass(<index of image>)} />
   const { allPhotos } = useContext(Context);
-  const imageElements = allPhotos.map((img, i) => {
-    <Image key={img.id} img={img} className={getClass(i)} />;
-  });
+
+  const imageElements = allPhotos.map((img, i) => (
+    <Image key={img.id} img={img} className={getClass(i)} />
+  ));
+
   return <main className="photos">{imageElements}</main>;
 }
 
