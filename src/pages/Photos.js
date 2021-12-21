28f484664c89abf9070306b05f44b31a679528ec
Puja0208 <pooja2081997@gmail.com>
2021-12-21 13:48:36 +0530

    feat: Extract photos from context and display them

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Image from '../components/Image';
 import { getClass } from '../utils';
+
+import { Context } from '../Context';
 
 function Photos() {
   // Get the allPhotos array from context
   // map over it, creating <Image /> elements of the component we just made
   // <Image key={???} img={<full image object here>} className={getClass(<index of image>)} />
-
-  return (
-    <main className="photos">
-      <h1>Images go here</h1>
-    </main>
-  );
+  const { allPhotos } = useContext(Context);
+  const imageElements = allPhotos.map((img, i) => {
+    <Image key={img.id} img={img} className={getClass(i)} />;
+  });
+  return <main className="photos">{imageElements}</main>;
 }
 
 export default Photos;
