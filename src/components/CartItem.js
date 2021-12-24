358574adbba08b95a5a0f6c4d5fb7d0756cbd4a5
Puja0208 <pooja2081997@gmail.com>
2021-12-24 12:41:40 +0530

    feat: Create custom useHover hook to store hovered property and use it on all elements

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,8 +1,10 @@
 import React, { useState, useContext } from 'react';
 import { Context } from '../Context.js';
+import PropTypes from 'prop-types';
+import useHover from '../hooks/useHover';
 
 function CartItem({ item }) {
-  const [hovered, setHovered] = useState(false);
+  const [hovered, ref] = useHover();
   const { removeFromCart } = useContext(Context);
   const iconClassName = hovered ? 'ri-delete-bin-fill' : 'ri-delete-bin-line';
   return (
@@ -10,8 +12,7 @@ function CartItem({ item }) {
       <i
         className={iconClassName}
         onClick={() => removeFromCart(item.id)}
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
+        ref={ref}
       ></i>
       <img src={item.url} width="130px" />
       <p>$5.99</p>
@@ -19,6 +20,10 @@ function CartItem({ item }) {
   );
 }
 
-export default CartItem;
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+  }),
+};
 
-import React from 'react';
+export default CartItem;
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,11 @@
 import React, { useState, useContext } from 'react';
 import { Context } from '../Context.js';
 import PropTypes from 'prop-types';
+import useHover from '../hooks/useHover';
 
 function Image({ className, img }) {
-  const [hovered, setHovered] = useState(false);
+  // const [hovered, setHovered] = useState(false);
+  const [hovered, ref] = useHover();
   const { toggleFavorite, addToCart, cartItems, removeFromCart } =
     useContext(Context);
   function heartIcon() {
@@ -44,11 +46,7 @@ function Image({ className, img }) {
   }
 
   return (
-    <div
-      className={`${className} image-container`}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
+    <div className={`${className} image-container`} ref={ref}>
       <img src={img.url} className="image-grid" />
       {heartIcon()}
       {cartIcon()}
